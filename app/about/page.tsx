e5ddf43d0ba9debd4e9cd19446ba126f65dd7b99
stevieismagic <stevieismagic@gmail.com>
2023-06-18 11:08:23 -0400

    update

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,14 +4,13 @@ export default function AboutPage() {
       <h1 className="font-bold text-3xl font-serif">About Me</h1>
       <p className="my-5 text-neutral-800 dark:text-neutral-200">
         Most folks online know me as <b>Stevie IsMagic</b>.
-        My pen name is a remnant from a time when it was unnatural to share personal details with strangers on the internet.
-        (Anyone remember the original meaning of a/s/l in those AOL chatrooms?) Times sure do change. Man my back hurts..
-        
+        The pen name is a remnant from a time when it was unnatural to share personal details with strangers on the internet.
+        (Anyone remember the original meaning of a/s/l in those early 2000's AOL chatrooms?) My back sure does hurt while writing this..
         
       </p>
       <div className="prose prose-neutral dark:prose-invert text-neutral-800 dark:text-neutral-200">
         <p>
-          I find writing to be one of the kindest forms of artistic expression. And I want write more about my journey as a human being.
+          I find writing to be one of the kindest forms of artistic expression. And I wish to share more of the knowledge I've aquired on my journey as a human being.
           Sir Isaac Newton once said that if today we see further than others, it is because we stand on the shoulders of giants.
           IMO, we each owe it to the world to share our knowledge and experiences. Could you imagine our predecesors never shared
           any of their ideas for the betterment of humanity? What kind of world would we live in today?
@@ -23,18 +22,18 @@ export default function AboutPage() {
           This combination of interests is what ultimately led me to my current professional role writing software.
           I've always felt a deep desire to have a seat at the roundtable of persons actively building the future.
           And I feel honored to live in an age where the tools to positively impact a generation are readily at my fingertips.
-          I most passionate about helping others learn and grow to their full potential - especially my mother and father. I literally owe them my life..
           
         </p>
         <hr />
         <p>
-          I'm currently working on a handful of projects that I'll <b>love</b> share with you soon enough.
+          I'm currently working on a handful of projects that I'll slowly share with you soon as I build out this site.
           My most important one involves helping my parents actualize their life dreams.
+          It's the least I can do for the two people who have literally given me their everything.
           Stay tuned for more details..
         </p>
         <p className="mb-8">
-          Outside of creative pursuits, I'm a <b>beach bum</b> and <b>aspiring yogi</b>.
-          The sea is a source of great peace and inspiration for me. I am, after all, a water sign. ~
+          Outside of creative pursuits, I'm a aspiring <b>beach bum</b> and <b>yogi</b>.
+          The sea is a source of great peace and inspiration for me. I am, after all, a water sign y'all. ~
         </p>
         </div>
     </section>
